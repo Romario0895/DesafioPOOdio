841eb960ae349d8d4bca570b8a0018386570edf2
git@github.com:Romario0895/lab-natty-or-not.git <rromario120@hotmail.com>
2024-07-10 06:40:14 -0300

    Mentoria + Curso

---
+++ src/br/com/dio/desadio/Curso.java
@@ -0,0 +1,44 @@
+package br.com.dio.desadio;
+
+public class Curso {
+    private String titulo;
+   private String descricao;
+    private int cargahoraria;
+
+    public Curso() {
+
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDesxricao() {
+        return descricao;
+    }
+
+    public void setDesxricao(String desxricao) {
+        this.descricao = descricao;
+    }
+
+    public int getCargahoraria() {
+        return cargahoraria;
+    }
+
+    public void setCargahoraria(int cargahoraria) {
+        this.cargahoraria = cargahoraria;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", cargahoraria=" + cargahoraria +
+                '}';
+    }
+}
